42e0e43b60eadd3b7fc49e5752ab97063a8ba24f
SlashGordon <slash.gordon.dev@gmail.com>
2024-10-25 13:35:30 +0200

    Refactor backtest.py for improved strategy flexibility and Telegram message handling

--- backtest.py
+++ backtest.py
@@ -5,8 +5,12 @@ from prettytable import PrettyTable
 from pystockfilter.tool.start_backtest import StartBacktest
 from pystockfilter.data.stock_data_source import DataSourceModule as Data
 from pystockfilter.strategy.rsi_strategy import RSIStrategy
+from pystockfilter.strategy.uo_strategy import UltimateStrategy
 from telegram import Bot
 from telegram.constants import ParseMode
+
+MESSAGE_SIZE_LIMIT = 4096  # Telegram message size limit
+CHUNK_SIZE = 50  # Adjust based on average row length to fit within limit
 
 def load_environment_variables():
     bot_token = os.getenv('BOT_TOKEN')
@@ -16,12 +20,14 @@ def load_environment_variables():
         raise ValueError("Please ensure both BOT_TOKEN and CHAT_ID are set as environment variables.")
     return bot_token, chat_id
 
-async def generate_backtest_results(data):
+async def generate_backtest_results(data, strategy_name):
     results = []
     for ticker, strategy_dict in data.items():
-        rsi_params = strategy_dict['RSIStrategy']["parameter"]
-        bt = StartBacktest([ticker], [RSIStrategy], [rsi_params], Data(source=Data.Y_FINANCE))
-        results.extend(bt.run())
+        if strategy_name in strategy_dict:
+            params = strategy_dict[strategy_name]["parameter"]
+            strategy_class = globals().get(strategy_name)
+            bt = StartBacktest([ticker], [strategy_class], [params], Data(source=Data.Y_FINANCE))
+            results.extend(bt.run())
     return results
 
 def save_results_to_file(results, filename):
@@ -50,29 +56,38 @@ def create_pretty_table(results):
         table.add_row(row)
     return table
 
-async def send_telegram_message(bot_token, chat_id, table):
+def split_results_into_chunks(results, chunk_size):
+    return [results[i:i + chunk_size] for i in range(0, len(results), chunk_size)]
+
+async def send_telegram_message(bot_token, chat_id, table, strategy_name, part):
     bot = Bot(token=bot_token)
     try:
-        headline_html = "<b>📊 RSIStrategy Backtest Results 📊</b>\n\n"
+        headline_html = f"<b>📊 {strategy_name} Backtest Results - Part {part} 📊</b>\n\n"
         await bot.send_message(
             chat_id=chat_id,
             text=f'{headline_html}<pre>{table}</pre>',
             parse_mode=ParseMode.HTML
         )
-        print("Messages sent successfully.")
+        print(f"{strategy_name} Part {part} messages sent successfully.")
     except Exception as e:
         print(f"Error: {e}")
 
-async def send_backtest_table():
-    """Generate a backtest results table and send it via Telegram."""
-    with open("parameter.json", "r") as json_file:
+async def send_backtest_tables(strategy_names):
+    """Generate backtest result tables for multiple strategies and send them via Telegram."""
+    with open("parameter_v2.json", "r") as json_file:
         data = json.load(json_file)
 
-    results = await generate_backtest_results(data)
-    sorted_results = save_results_to_file(results, 'backtest_results.json')
-    table = create_pretty_table(sorted_results)
     bot_token, chat_id = load_environment_variables()
-    await send_telegram_message(bot_token, chat_id, table)
+
+    for strategy_name in strategy_names:
+        results = await generate_backtest_results(data, strategy_name)
+        sorted_results = save_results_to_file(results, f'backtest_results_{strategy_name}.json')
+        result_chunks = split_results_into_chunks(sorted_results, CHUNK_SIZE)
+
+        for i, chunk in enumerate(result_chunks, 1):
+            table = create_pretty_table(chunk)
+            await send_telegram_message(bot_token, chat_id, table, strategy_name, i)
 
 if __name__ == "__main__":
-    asyncio.run(send_backtest_table())
+    strategy_names =  os.getenv('STRATEGY_NAMES', "UltimateStrategy,RSIStrategy").split(",")
+    asyncio.run(send_backtest_tables(strategy_names))
